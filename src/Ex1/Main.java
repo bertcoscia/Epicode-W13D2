dcd80d1d4171915fb99a17b6a3fe10a53f2a7ac6
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-07-23 15:45:01 +0200

    ex1.2

--- src/Ex1/Main.java
+++ src/Ex1/Main.java
@@ -10,8 +10,26 @@ public class Main {
         }
     }
 
+    public static boolean annoBisestile(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
     public static void main(String[] args){
         boolean result1 = stringaPariDispari("ciao come va");
         System.out.println(result1);
+        boolean result2 = annoBisestile(1600);
+        System.out.println(result2);
     }
 }
