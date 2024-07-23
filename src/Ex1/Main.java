153dfcf49ddc6b88ccedf95c53db77c5a258a719
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-07-23 15:31:09 +0200

    ex1.1

--- src/Ex1/Main.java
+++ src/Ex1/Main.java
@@ -1,4 +1,17 @@
 package Ex1;
 
 public class Main {
+
+    public static boolean stringaPariDispari(String str) {
+        if(str.length() % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args){
+        boolean result1 = stringaPariDispari("ciao come va");
+        System.out.println(result1);
+    }
 }
